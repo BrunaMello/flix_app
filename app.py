53adc7cf6016be039d9fb463cc64d0831d3eb04a
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-06-02 11:09:55 +0100

    003 - Telas e menu

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 import streamlit as st
 
-st.title('Flix App')
-st.divider()
-st.text_input('Movie Title:')
-st.button('Save')
+
+def main():
+	st.title("Flix App")
+
+	menu_options = st.sidebar.selectbox(
+		'Select one option',
+		['Home', 'Genre', 'Actors', 'Movies', 'Rate']
+	)
 
 
+	if menu_options == 'Home':
+		st.write('Welcome to Flix App')
+
+	if menu_options == 'Genre':
+		st.write('Genre')
+
+	if menu_options == 'Actors':
+		st.write('Actors')
+
+	if menu_options == 'Movies':
+		st.write('Movies')
+
+	if menu_options == 'Rate':
+		st.write('Rate')
+
+if __name__ == '__main__':
+	main()
