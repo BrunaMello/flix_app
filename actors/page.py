663a8e71a26c504f0683905b6125efd7e3d2bbf4
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-06-03 12:37:34 +0100

    017 - Finalizando integração de actors

--- actors/page.py
+++ actors/page.py
@@ -1,21 +1,52 @@
+from datetime import datetime
+
 import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid
 
-file_path = 'actors/actors.csv'
-actors = pd.read_csv(file_path)
+from actors.service import ActorService
 
 
 def show_actors():
-	st.write('Actors List:')
+	actors_service = ActorService()
+	actors = actors_service.get_actors()
 
-	AgGrid(
-		data=actors,
-		reload_data=True,
-		key='genres_grid',
-	)
+	if actors:
+		st.write('Actors List:')
+
+		actors_df = pd.json_normalize(actors)
+
+		AgGrid(
+			data=actors_df,
+			reload_data=True,
+			key='actors_grid',
+		)
+	else:
+		st.warning('No actors found')
 
 	st.title('Add a new actor')
+
 	name = st.text_input('Actor name')
+	birthday = st.date_input(
+		label='Birthday',
+		value=datetime.today(),
+		min_value=datetime(1600, 1, 1),
+		max_value=datetime.today(),
+		format='DD-MM-YYYY',
+	)
+	nationality_dropdown = ['BRAZIL', 'USA']
+	nationality = st.selectbox(
+		label='Nationality',
+		options=nationality_dropdown,
+	)
+
 	if st.button('Save'):
-		st.success(f'Actor "{name}" saved successfully')
+		new_actor = actors_service.create_actor(
+			name=name,
+			birthday=birthday,
+			nationality=nationality,
+		)
+		if new_actor:
+			st.rerun()
+		else:
+			st.error('Error creating actor')
