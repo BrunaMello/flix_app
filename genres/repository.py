1fa1f2d0ac304a020af15b06b0688b336983a88c
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-06-03 11:16:42 +0100

    012 - Criando repository de genres (parte 2)

--- genres/repository.py
+++ genres/repository.py
@@ -26,3 +26,16 @@ class GenreRepository:
 			logout()
 			return None
 		raise Exception(f'Error getting genres. Status Code: {response.status_code}')
+
+	def create_genre(self, genre):
+		response = requests.post(
+			self.__genres_url,
+			headers=self.__headers,
+			data=genre,
+		)
+		if response.status_code == 201:
+			return response.json()
+		if response.status_code == 401:
+			logout()
+			return None
+		raise Exception(f'Error getting genres. Status Code: {response.status_code}')
